f4ffd74f2998cbc0ea6d027ed2b0cfd30b52528e
hfy <2782668733@qq.com>
2023-01-03 15:26:20 +0800

    OLED

--- JY901/MDK-ARM/JY901/jy901.d
+++
@@ -1,4 +0,0 @@
-jy901/jy901.o: ..\Core\Src\jy901.c ..\Core\Inc\jy901.h \
-  D:\MDK5\core\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  D:\MDK5\core\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  D:\MDK5\core\ARM\ARMCLANG\Bin\..\include\string.h
